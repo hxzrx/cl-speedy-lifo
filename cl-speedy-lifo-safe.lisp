cc52fe9ed27b16d7ac81f41f022decd165aff420
hxz <herongxian@163.com>
2022-04-22 23:35:14 +0800

    not tested

--- cl-speedy-lifo-safe.lisp
+++ cl-speedy-lifo-safe.lisp
@@ -1,24 +1,19 @@
 (in-package #:cl-speedy-lifo-safe)
 
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *dummy* '!dummy!))
+  (defconstant *dummy* '!dummy!)
+  (defconstant *max-queue-length* (expt 10 10)))
 
 
 (define-speedy-function %make-queue (length)
   "Creates a new queue of maximum size LENGTH"
-  (when (typep length 'fixnum)
-    (locally (declare (fixnum length))
-      (when (plusp length)
-        (let ((queue (make-array (the fixnum (+ 1 length)) :initial-element nil)))
-          ;; the 0th place, the queue's entry pointer,
-          ;; stores the index of the top of the lifo queue, and (= (svref queue 0) 0) shows an empty queue
-          (setf (svref queue 0) 0)
-          (return-from %make-queue queue)))))
-  (error 'queue-length-error :attempted-length length))
-
-;;; Do we need a compiler macro for the above when LENGTH is constant so that we
-;;;   don't add 2 at runtime? That's not very high on the priority list, although
-;;;   it'll probably take less time to write than this comment did. -- Adlai
+  (declare (fixnum length))
+  (let ((queue (make-array (the fixnum (+ 1 length)) :initial-element '#.*dummy*)))
+    ;; the 0th place, the queue's entry pointer,
+    ;; stores the index of the top of the lifo queue, and (= (svref queue 0) 0) shows an empty queue
+    (setf (svref queue 0) 0)
+    queue))
 
 (define-speedy-function %queue-length (queue)
   "Returns QUEUE's maximum length"
@@ -27,27 +22,23 @@
 (define-speedy-function queuep (x)
   "If this returns NIL, X is not a queue"
   (when (simple-vector-p x)
-    (let ((length (length x))
-          (head (svref x 0)))
-      (and (typep head 'fixnum)
-           (< 1 head length)))))
+    (let ((length (the fixnum (length x)))
+          (head (the fixnum (svref x 0))))
+      (< -1 head length #.(1+ *max-queue-length*)))))
 
 (define-speedy-function %queue-out (queue)
   "QUEUE's exit pointer"
+  (declare (simple-vector queue))
   (the fixnum (svref queue 0)))
 
 (define-speedy-function %queue-in (queue)
   "QUEUE's entry pointer" ; overflow will be checked when this function is called
+  (declare (simple-vector queue))
   (the fixnum (1+ (the fixnum (svref queue 0)))))
 
 (define-speedy-function %queue-peek (queue)
   "Dereference QUEUE's exit pointer" ; underflow will be checked when this function is called
   (svref queue (%queue-out queue)))
-
-#+:ignore
-(define-speedy-function %queue-zero-p (queue)
-  "Checks whether QUEUE's theoretical length is zero"
-  (= (the fixnum (svref queue 0)) 0))
 
 (define-speedy-function %queue-empty-p (queue)
   "Checks whether QUEUE is effectively empty"
@@ -67,37 +58,55 @@
   (declare (fixnum current-index))
   (the fixnum (1+ current-index)))
 
-(define-speedy-function %enqueue (object queue &aux (in (%queue-in queue)))
-  (declare (fixnum in))
-  "Enqueue OBJECT and increment QUEUE's entry pointer"
-  (if (< in (the fixnum (length queue))) ;(/= in (the fixnum (length queue))) ; 02/24
-      (prog1 (setf (svref queue in) object)
-        (incf (the fixnum (svref queue 0))))
-      ;;(error 'queue-overflow-error :queue queue :item object)
-      #.*overflow-flag*))
+(define-speedy-function %enqueue (object queue)
+  "Enqueue OBJECT and increment QUEUE's entry pointer, thread safe."
+  (let ((len (the fixnum (length (the simple-array queue)))))
+    (loop (let* ((entry (the fixnum (svref queue 0)))
+                 (new-entry (1+ entry)))
+            (if (< new-entry len)
+                (when (atomics:cas (svref queue 0) entry new-entry)
+                  ;; the following setf will not overwrite for a fifo type and thus it will always success,
+                  ;; but the action may be taken later
+                  (return (setf (svref queue new-entry) object)))
+                ;; full, enqueue will overflow, cas to make sure it's really full
+                (when (atomics:cas (svref queue 0) entry entry) ; enqueue to a full queue is idempotent
+                  (return #.*overflow-flag*)))))))
 
-(define-speedy-function %dequeue (queue keep-in-queue-p &aux (out (%queue-out queue)))
-  "DEQUEUE, decrements QUEUE's entry pointer, and returns the previous top ref"
-  (declare (fixnum out))
-  (declare (boolean keep-in-queue-p))
-  (if (/= (the fixnum (svref queue 0)) 0)
-      (prog1 (svref queue out)
-        (unless keep-in-queue-p (setf (svref queue out) nil))
-        (decf (the fixnum (svref queue 0))))
-      ;;(error 'queue-underflow-error :queue queue)
-      #.*underflow-flag*))
+(define-speedy-function %dequeue (queue)
+  "DEQUEUE, decrements QUEUE's entry pointer, and returns the previous top ref, thread safe."
+  (loop (let* ((out (the fixnum (svref queue 0)))
+               (new-out (1- out)))
+          (if (= out 0)
+              ;; empty, dequeue will underflow, cas to make sure it's really empty
+              (when (atomics:cas (svref queue 0) out out) ; out == 0, dequeue to an empty queue is idempotent
+                (return #.*underflow-flag*))
+              ;; since the setf action for this place may be taken future in some time,
+              ;; the svref action should make sure this place has been setf successfully,
+              ;; so, the dummy checking here is necessary,
+              ;; as that place may not have been written by the enqueue operation at the current time.
+              (when (and (null (eq (svref queue out) '#.*dummy*))
+                         (atomics:cas (svref queue 0) out new-out))
+                (let ((res '#.*dummy*))
+                  (rotatef (svref queue out) res)
+                  (return res)))))))
 
 (define-speedy-function %queue-flush (queue)
-  "Returns QUEUE's effective length"
-  (setf (svref queue 0) 0)
-  queue)
+  (loop for i = (%dequeue queue)
+        until (%queue-empty-p queue)
+        finally (return queue)))
 
 ;;; Now that all the backend functions are defined, we can define the API:
 
 (defun make-queue (size)
   "Makes a queue of maximum size SIZE"
-  (declare (fixnum size))
-  (%make-queue size))
+  (assert (integerp size))
+  (if (<= 2 size #.*max-queue-length*)
+      (%make-queue size)
+      (if (> size #.*max-queue-length*)
+          (progn (warn "The size ~d exceeds the limit, will make a new queue with max size ~d" size #.*max-queue-length*)
+                 (%make-queue #.*max-queue-length*))
+          (error 'queue-length-error :attempted-length size))))
+
 
 (defun queue-count (queue)
   "Returns the current size of QUEUE"
@@ -136,7 +145,7 @@
   "Convert a list to a LIFO queue, with the popping order kept."
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (declare (list lst))
-  (let ((queue (make-lifo (length lst))))
+  (let ((queue (make-queue (length lst))))
     (dolist (item (reverse lst))
       (%enqueue item queue))))
 
@@ -145,17 +154,11 @@
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (%enqueue object queue))
 
-(defun dequeue (queue &optional (keep-in-queue-p t))
-  "Dequeues QUEUE. Return the element dequed.
-
-When `keep-in-queue-p' sets to nil, the dequeued val will no longer keep an ref in the queue,
-this is useful when the queue holds very big objects.
-
-Making `keep-in-queue-p' as an optional parameter bring some performance penalty,
-about 5 seconds slower than that as a non-optional parameter in a 10^9 pushes+pop operations test,
-but it's still very fast."
+(defun dequeue (queue &optional keep-in-queue-p)
+  "Dequeues QUEUE. Return the element dequed."
   (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (%dequeue queue keep-in-queue-p))
+  (declare (ignore keep-in-queue-p))
+  (%dequeue queue))
 
 (defun queue-find (item queue &key (key #'identity) (test #'eql))
   "Find `item' in `queue'"
@@ -167,43 +170,6 @@ but it's still very fast."
         (find item queue :start 1 :end in :key key :test test))))
 
 (defun queue-flush (queue)
-  "Make `queue' empty"
+  "Make `queue' empty, ONLY use it when no other threads are modifying the `queue'."
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (%queue-flush queue))
-
-
-;; ------- thread safe version -------
-
-(define-speedy-function %enqueue-safe (object queue)
-  "Enqueue OBJECT and increment QUEUE's entry pointer, thread safe."
-  (let ((len (length (the simple-array queue))))
-    (loop (let* ((entry (the fixnum (svref queue 0)))
-                 (new-entry (1+ entry)))
-            (if (< new-entry len)
-                (when (compare-and-swap (svref queue 0) entry new-entry)
-                  (setf (svref queue new-entry) object)
-                  (return object))
-                (return #.*overflow-flag*))))))
-
-(define-speedy-function %dequeue-safe (queue keep-in-queue-p)
-  "DEQUEUE, decrements QUEUE's entry pointer, and returns the previous top ref, thread safe."
-  (loop (let* ((out (the fixnum (svref queue 0)))
-               (new-out (1- out)))
-          (if (/= out 0)
-              (when (compare-and-swap (svref queue 0) out new-out)
-                (if keep-in-queue-p
-                    (return (svref queue out))
-                    (let ((out-val nil))
-                      (rotatef (svref queue out) out-val)
-                      (return out-val))))
-              (return #.*underflow-flag*)))))
-
-(defun enqueue-safe (object queue)
-  "Thread safe version of enqueue."
-  (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (%enqueue-safe object queue))
-
-(defun dequeue-safe (queue &optional (keep-in-queue-p t))
-  "Thread safe version of dequeue."
-  (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (%dequeue-safe queue keep-in-queue-p))
