b8440d0061ba83610ba371ee4f07ec8c822c2bbf
hxz <herongxian@163.com>
2022-04-24 11:24:57 +0800

    update

--- perform.lisp
+++ perform.lisp
@@ -1,28 +1,61 @@
-(in-package :cl-speedy-lifo)
+(ql:quickload :cl-speedy-lifo)
 
 
 (defparameter *times* (loop for i from 3 to 9
                             collect (expt 10 i)))
 
+
+;; ------- unsafe version -------
+
+;; 13.112 seconds, 10^9, sbcl
 (dolist (num *times*)
   (format t "LIFO queue, push+pop, without consing timed: 10^~d times.~%" (log num 10))
   (sb-ext:gc :full t)
-  (let ((lifo-queue (make-queue num)))
+  (let ((lifo-queue (cl-speedy-lifo:make-queue num)))
     (time (progn
             (dotimes (i num)
-              (enqueue i lifo-queue))
+              (cl-speedy-lifo:enqueue i lifo-queue))
             (dotimes (i num)
-              (dequeue lifo-queue nil))))))
+              (cl-speedy-lifo:dequeue lifo-queue nil))))))
 
+;; 15.244 seconds, 10^9, sbcl
 (dolist (num *times*)
   (format t "LIFO queue, push+pop, with consing timed: 10^~d times.~%" (log num 10))
   (sb-ext:gc :full t)
-  (time (let ((lifo-queue (make-queue num)))
+  (time (let ((lifo-queue (cl-speedy-lifo:make-queue num)))
           (dotimes (i num)
-            (enqueue i lifo-queue))
+            (cl-speedy-lifo:enqueue i lifo-queue))
           (dotimes (i num)
-            (dequeue lifo-queue nil)))))
+            (cl-speedy-lifo:dequeue lifo-queue nil)))))
 
+
+;; ------- safe version -------
+
+;; 26.836 seconds, 10^9, sbcl
+(dolist (num *times*)
+  (format t "LIFO queue, push+pop, without consing timed: 10^~d times.~%" (log num 10))
+  (sb-ext:gc :full t)
+  (let ((lifo-queue (cl-speedy-lifo-safe:make-queue num)))
+    (time (progn
+            (dotimes (i num)
+              (cl-speedy-lifo-safe:enqueue i lifo-queue))
+            (dotimes (i num)
+              (cl-speedy-lifo-safe:dequeue lifo-queue nil))))))
+
+;; 29.468 seconds, 10^9, sbcl
+(dolist (num *times*)
+  (format t "LIFO queue, push+pop, with consing timed: 10^~d times.~%" (log num 10))
+  (sb-ext:gc :full t)
+  (time (let ((lifo-queue (cl-speedy-lifo-safe:make-queue num)))
+          (dotimes (i num)
+            (cl-speedy-lifo-safe:enqueue i lifo-queue))
+          (dotimes (i num)
+            (cl-speedy-lifo-safe:dequeue lifo-queue nil)))))
+
+
+;; -------- cl list -------
+
+;; 1.636 seconds, 10^8, sbcl. For 10^9, it failed to get a result.
 (dolist (num *times*)
   (format t "LIST queue, push+pop: 10^~d times.~%" (log num 10))
   (sb-ext:gc :full t)
